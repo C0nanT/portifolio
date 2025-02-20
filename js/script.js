0bf5eace62a2a9110c77abe8d49ba1fcaccdd094
Conan Torres <conantorreswf@gmail.com>
2025-02-20 19:22:46 -0300

    feat: format

--- js/script.js
+++ js/script.js
@@ -60,83 +60,116 @@ function adjustNavDisplay() {
 	}
 }
 
-function aplicarEfeitoHover(ativadorSelector, classesAlvoSelectors, estilosHover, estiloPadrao) {
-    const ativador = document.querySelector(ativadorSelector);
-    const classesAlvo = classesAlvoSelectors.map(selector => document.querySelector(selector));
-    const todosBgElements = document.querySelectorAll('.empresa-sistemapet [class$="-bg"]');
+function aplicarEfeitoHover(
+	ativadorSelector,
+	classesAlvoSelectors,
+	estilosHover,
+	estiloPadrao
+) {
+	const ativador = document.querySelector(ativadorSelector);
+	const classesAlvo = classesAlvoSelectors.map((selector) =>
+		document.querySelector(selector)
+	);
+	const todosBgElements = document.querySelectorAll(
+		'.empresa-sistemapet [class$="-bg"]'
+	);
 
-    todosBgElements.forEach(el => {
-        el.dataset.originalDisplay = window.getComputedStyle(el).display;
-    });
+	todosBgElements.forEach((el) => {
+		el.dataset.originalDisplay = window.getComputedStyle(el).display;
+	});
 
-    function aplicarEstilos(elemento, estilos) {
-        Object.assign(elemento.style, estilos);
-    }
+	function aplicarEstilos(elemento, estilos) {
+		Object.assign(elemento.style, estilos);
+	}
 
-    ativador.addEventListener('mouseover', () => {
-        aplicarEstilos(document.querySelector('.empresa'), { backgroundColor: estilosHover.backgroundColor });
+	ativador.addEventListener("mouseover", () => {
+		aplicarEstilos(document.querySelector(".empresa"), {
+			backgroundColor: estilosHover.backgroundColor,
+		});
 
-        classesAlvo.forEach((elemento, index) => {
-            if (elemento) {
-                aplicarEstilos(elemento, estilosHover[Object.keys(estilosHover)[index + 1]]);
-                elemento.style.display = elemento.dataset.originalDisplay;
-            }
-        });
-    });
+		classesAlvo.forEach((elemento, index) => {
+			if (elemento) {
+				aplicarEstilos(
+					elemento,
+					estilosHover[Object.keys(estilosHover)[index + 1]]
+				);
+				elemento.style.display = elemento.dataset.originalDisplay;
+			}
+		});
+	});
 
-    ativador.addEventListener('mouseout', () => {
-        aplicarEstilos(document.querySelector('.empresa'), { backgroundColor: estiloPadrao.backgroundColor });
+	ativador.addEventListener("mouseout", () => {
+		aplicarEstilos(document.querySelector(".empresa"), {
+			backgroundColor: estiloPadrao.backgroundColor,
+		});
 
-        todosBgElements.forEach(el => {
-            aplicarEstilos(el, estiloPadrao);
-            el.style.display = el.dataset.originalDisplay;
-        });
-    });
+		todosBgElements.forEach((el) => {
+			aplicarEstilos(el, estiloPadrao);
+			el.style.display = el.dataset.originalDisplay;
+		});
+	});
 }
 
 const estilosHoverChatbot = {
-    backgroundColor: '#f5f5f5',
-    php: { backgroundColor: '#4c4e73', color: '#fff' },
-    typescript: { backgroundColor: '#17395d', color: '#fff' },
-    mysql: { backgroundColor: '#00758f', color: '#fff' },
-    laravel: { backgroundColor: '#ff2d20', color: '#fff' },
-    react: { backgroundColor: '#306d7e', color: '#fff' },
-    next: { backgroundColor: '#000000', color: '#fff' },
-    python: { backgroundColor: '#18354d', color: '#fff' },
-    git: { backgroundColor: '#141414', color: '#fff' }
+	backgroundColor: "#f5f5f5",
+	php: { backgroundColor: "#4c4e73", color: "#fff" },
+	typescript: { backgroundColor: "#17395d", color: "#fff" },
+	mysql: { backgroundColor: "#00758f", color: "#fff" },
+	laravel: { backgroundColor: "#ff2d20", color: "#fff" },
+	react: { backgroundColor: "#306d7e", color: "#fff" },
+	next: { backgroundColor: "#000000", color: "#fff" },
+	python: { backgroundColor: "#18354d", color: "#fff" },
+	git: { backgroundColor: "#141414", color: "#fff" },
 };
 
 const estilosHoverAppMobile = {
-    backgroundColor: '#f5f5f5',
-    php: { backgroundColor: '#4c4e73', color: '#fff' },
-    mysql: { backgroundColor: '#00758f', color: '#fff' },
-    laravel: { backgroundColor: '#ff2d20', color: '#fff' },
-    reactnative: { backgroundColor: '#306d7e', color: '#fff' },
-    git: { backgroundColor: '#141414', color: '#fff' },
-    typescript: { backgroundColor: '#17395d', color: '#fff' },
-    figma: { backgroundColor: '#f24e1e', color: '#fff' },
-    apache: { backgroundColor: '#cc3333', color: '#fff' }
+	backgroundColor: "#f5f5f5",
+	php: { backgroundColor: "#4c4e73", color: "#fff" },
+	mysql: { backgroundColor: "#00758f", color: "#fff" },
+	laravel: { backgroundColor: "#ff2d20", color: "#fff" },
+	reactnative: { backgroundColor: "#306d7e", color: "#fff" },
+	git: { backgroundColor: "#141414", color: "#fff" },
+	typescript: { backgroundColor: "#17395d", color: "#fff" },
+	figma: { backgroundColor: "#f24e1e", color: "#fff" },
+	apache: { backgroundColor: "#cc3333", color: "#fff" },
 };
 
 const estiloPadrao = {
-    backgroundColor: '#fff',
-    color: '#000'
+	backgroundColor: "#fff",
+	color: "#000",
 };
 
 aplicarEfeitoHover(
-    '.chatbot',
-    ['.php-bg', '.typescript-bg', '.mysql-bg', '.laravel-bg', '.react-bg', '.next-bg', '.python-bg', '.git-bg'],
-    estilosHoverChatbot,
-    estiloPadrao
+	".chatbot",
+	[
+		".php-bg",
+		".typescript-bg",
+		".mysql-bg",
+		".laravel-bg",
+		".react-bg",
+		".next-bg",
+		".python-bg",
+		".git-bg",
+	],
+	estilosHoverChatbot,
+	estiloPadrao
 );
 
 aplicarEfeitoHover(
-    '.sistemapet-criadores',
-    ['.php-bg', '.mysql-bg', '.laravel-bg', '.react-native-bg', '.git-bg', '.typescript-bg', '.figma-bg', '.apache-bg'],
-    estilosHoverAppMobile,
-    estiloPadrao
+	".sistemapet-criadores",
+	[
+		".php-bg",
+		".mysql-bg",
+		".laravel-bg",
+		".react-native-bg",
+		".git-bg",
+		".typescript-bg",
+		".figma-bg",
+		".apache-bg",
+	],
+	estilosHoverAppMobile,
+	estiloPadrao
 );
-
 
 document.addEventListener("DOMContentLoaded", adjustNavDisplay);
 window.addEventListener("resize", adjustNavDisplay);
